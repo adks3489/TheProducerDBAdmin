f2c25abf3678e8798c6f65365aca81392a4fd7e6
James Chiang <adks3489@gmail.com>
2018-12-29 16:49:45 +0800

    增加腳本管理API

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const ScriptManager = require('./lib/scriptManager');
 
 const app = express();
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('OK');
+  res.send('');
 });
+
+let scriptManager = new ScriptManager(app, '/script');
 
 app.listen(80, () => {
   console.log('web server listening on port 80');
---
+++ Server/lib/scriptManager.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const PROPERTY = "scripts";
+class ScriptManager {
+  /**
+   * Constructor
+   * @param {express} express
+   * @param {string} path
+   */
+  constructor(app, path) {
+    const adapter = new FileSync('data/articles.json');
+    this.db = low(adapter);
+    this.db.defaults({ scripts: [], lastKey: -1 }).write();
+    this.collection = this.db.get(PROPERTY);
+
+    app.get(`${path}`, this.list.bind(this));
+    app.get(`${path}/:id`, (req, res) => {
+      res.send(this.query(req.params.id));
+    });
+    app.use(`${path}/:id`, express.json());
+    app.put(`${path}/:id`, (req, res) => {
+      this.update(req.params.id, req.body);
+      res.send('OK');
+    });
+  }
+
+  /**
+   * @param {express.Request} req
+   * @param {express.Response} res
+   */
+  list(req, res) {
+    res.send(this.collection.value());
+  }
+
+  query(key) {
+    return this.collection.find({key: key}).value();
+  }
+
+  add(value) {
+    let lastKey = this.db.get('lastKey').value() + 1;
+    this.collection.push({key: lastKey, ...value}).write();
+    this.db.get('lastKey').assign(lastKey).write();
+  }
+
+  update(key, value) {
+    this.collection.find({key: key}).assign(value).write();
+  }
+
+  delete(key) {
+    this.collection.remove({key: key}).write();
+  }
+}
+
+module.exports = ScriptManager;
